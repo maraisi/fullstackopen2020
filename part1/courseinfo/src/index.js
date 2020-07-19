43debf459f611df6006337f6d1455cfba7b1dba6
maraisi <25108577+maraisi@users.noreply.github.com>
2020-07-19 15:10:31 -0300

    Add files via upload
    exercise 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,50 +1,60 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const Header = (props) => {
 	return (
-		<p> {props.course} </p>
+		<p>{props.course}</p>
+	)	 
+}
+
+const Part = (props) => {
+	return (
+		<p>{props.part} {props.exercise}</p>
 	)
 }
 
 const Content = (props) => {
+
 	return (
 		<div>
-			<p> {props.part1} {props.exercise1} </p>
-			<p> {props.part2} {props.exercise2} </p>
-			<p> {props.part3} {props.exercise3} </p>
+			< Part part={props.p1} exercise={props.ex1} />
+			< Part part={props.p2} exercise={props.ex2} />
+			< Part part={props.p3} exercise={props.ex3} />
 		</div>
 	)
 }
 
+
 const Total = (props) => {
 	return (
 		<p> Number of exercises {props.total} </p>
-	)
+	)	
 }
 
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+	const course = "Half Stack application development"
+	const part1 = "Fundamentals of React"
+	const exercises1 = 10
+	const part2 = "Using props to pass data"
+	const exercises2 = 7
+	const part3 = "State of a component"
+	const exercises3 = 14
+	
 
-  return (
-    <div>
-      <Header course={course} />
-      
-		<Content 
-			part1={part1} exercise1={exercises1}
-			part2={part2} exercise2={exercises2}
-			part3={part3} exercise3={exercises3}
-		/>
+	return (
+		<div>
 
-      <Total total={exercises1 + exercises2 + exercises3} />
-    </div>
-  )
+			<Header course={course}/>
+			<Content 
+				p1={part1} ex1={exercises1}
+				p2={part2} ex2={exercises2}
+				p3={part3} ex3={exercises3}
+			/>
+			<Total total={exercises1 + exercises2 + exercises3} />
+
+		</div>
+	)
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
